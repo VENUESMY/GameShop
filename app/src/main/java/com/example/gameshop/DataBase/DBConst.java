36c46e2a083a6abffac313017087dd79339f56f1
user <@uy>
2025-03-20 16:51:51 +0500

    Изменил DBConst, DBHelper

--- app/src/main/java/com/example/gameshop/DataBase/DBConst.java
+++ app/src/main/java/com/example/gameshop/DataBase/DBConst.java
@@ -1,4 +1,15 @@
 package com.example.gameshop.DataBase;
 
 public class DBConst {
+
+    public static final String DATABASE_NAME = "GameShop.db";
+    public static final int DATABASE_VERSION = 1;
+    public static final String GAME_TABLE_NAME = "games";
+    public static final String GAME_ID = "id";
+    public static final String GAME_NAME = "name";
+    public static final String GAME_DESCRIPTION = "description";
+    public static final String GAME_PRICE = "price";
+    public static final String GAME_ID_DEVELOPER = "idDeveloper";
+
+
 }
--- app/src/main/java/com/example/gameshop/DataBase/DBHelper.java
+++ app/src/main/java/com/example/gameshop/DataBase/DBHelper.java
@@ -1,4 +1,23 @@
 package com.example.gameshop.DataBase;
 
-public class DBHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(@Nullable Context context) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
