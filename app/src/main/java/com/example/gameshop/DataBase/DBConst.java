a689ab808e847c0aab2715154d50db0d6cca4be8
user <@uy>
2025-03-20 17:02:24 +0500

    Изменил DBConst

--- app/src/main/java/com/example/gameshop/DataBase/DBConst.java
+++ app/src/main/java/com/example/gameshop/DataBase/DBConst.java
@@ -17,4 +17,18 @@ public class DBConst {
 
     //Developers
     public static final String DEVELOPER_TABLE_NAME = "developers";
+    public static final String DEVELOPER_ID = "id";
+    public static final String DEVELOPER_NAME = "name";
+    public static final String DEVELOPER_DESCRIPTION = "description";
+
+    //Purchases
+    public static final String PURCHASE_TABLE_NAME = "purchases";
+    public static final String PURCHASE_ID = "id";
+    public static final String PURCHASE_DATE = "date";
+    public static final String PURCHASE_TOTAL_PRICE = "totalPrice";
+
+    //Categories
+    public static final String CATEGORY_TABLE_NAME = "category";
+    public static final String CATEGORY_ID = "id";
+    public static final String CATEGORY_NAME = "name";
 }
