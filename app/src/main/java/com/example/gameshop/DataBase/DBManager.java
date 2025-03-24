477db58e13644af2005c0fc76af2bedf0b7096d4
user <@uy>
2025-03-24 17:16:17 +0500

    Изменил

--- app/src/main/java/com/example/gameshop/DataBase/DBManager.java
+++ app/src/main/java/com/example/gameshop/DataBase/DBManager.java
@@ -1,4 +1,14 @@
 package com.example.gameshop.DataBase;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteOpenHelper;
+
 public class DBManager {
+
+    private DBHelper dbHelper;
+    private SQLiteOpenHelper sqLiteOpenHelper;
+
+    public DBManager (Context context){
+        dbHelper = new DBHelper(context);
+    }
 }
